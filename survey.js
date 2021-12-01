de1031c7d85b97f47bae419b7d79162749418933
CUI <garycui222@gmail.com>
2021-11-30 22:28:19 -0500

    first try

---
+++ survey.js
@@ -0,0 +1,36 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+rl.question('What do you think of Node.js? ', (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+  rl.question("What's an activity you like doing?", (answer) => {
+    console.log(`Thank you for your valuable feedback: ${answer}`);
+  
+    rl.question("What do you listen to while doing that?", (answer) => {
+      console.log(`Thank you for your valuable feedback: ${answer}`);
+    
+      rl.question("Which meal is your favourite", (answer) => {
+        console.log(`Thank you for your valuable feedback: ${answer}`);
+      
+        rl.question("What's your favourite thing to eat for that meal?", (answer) => {
+          console.log(`Thank you for your valuable feedback: ${answer}`);
+        
+          rl.question("Which sport is your absolute favourite?", (answer) => {
+            console.log(`Thank you for your valuable feedback: ${answer}`);
+          
+            rl.question("What is your superpower?", (answer) => {
+              console.log(`Thank you for your valuable feedback: ${answer}`);
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
+});
